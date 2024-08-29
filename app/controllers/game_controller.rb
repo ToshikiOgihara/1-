845676d08093346abd693047d64b06e1e001c07b
Toshiki_Ogihara <toshiki.ogihara14@gmail.com>
2024-08-29 00:43:33 +0000

    配牌を14枚並べる

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,2 +1,11 @@
 class GameController < ApplicationController
+  def test
+    majon_pais = [
+      %w[m p s].product([*1..9]).map{ ["#{_1}#{_2}"] * 4 },
+      (1..7).map{ ["z#{_1}"] * 4 }
+    ].flatten.freeze()
+
+    haipai = majon_pais.sample(14)
+    @ripai = haipai
+  end
 end

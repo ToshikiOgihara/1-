3451f6e920ecd88c47db6a3dfd90cfd6c23b4eb6
Toshiki_Ogihara <toshiki.ogihara14@gmail.com>
2024-10-04 11:13:21 +0000

    クリックした時のイベントを実装

--- app/javascript/mahjong.js
+++ app/javascript/mahjong.js
@@ -4,3 +4,19 @@ document.getElementById('hogeButton').addEventListener('click', e => {
   OnSubmitClick();
 });
 */
+
+/*
+document.getElementsByClassName('hand-tile').addEventListener('click', e => {
+    //OnSubmitClick();
+    alert("hello")
+})
+*/
+
+
+
+document.querySelectorAll(".hand-tile").forEach(function(elem){
+    elem.addEventListener('click', () => {
+       //何かしらの処理
+       alert("id番号:" + elem.id);
+    })
+})
